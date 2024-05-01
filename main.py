aba86f9cce1ee1a3899af6c917928398efbb8d7c
sehaj punit singh <ssingh5@unm.edu>
2024-04-30 20:40:47 -0700

    Finished the project with front page, titles, and icons.

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+import streamlit as st
+import pandas
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+st.set_page_config(layout="wide");
+col1, col2 = st.columns(2)
 
+with col1:
+    st.image("images/SehajSingh.png", width=400)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+with col2:
+    st.title('Sehaj Punit Singh')
+    content = """
+      Hello, this is Sehaj!!
+      """
+    st.info(content) #blue box around text
+    #st.write(content)
 
+content2 = """
+Below you will find some stuff
+"""
+st.info(content2)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#creating columns and adding space b/w them
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
+df = pandas.read_csv("data.csv",sep=";")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with col3:
+    for index, row in df.iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+# col3 and col 4 do the same things except
+# creating two different columns to divide up
+#'title', 'description, 'image', 'url' are
+#column names inside df for each of the 19 entries
+#row refers to that object
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+
